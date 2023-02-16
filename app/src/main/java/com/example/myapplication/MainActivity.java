8ee2c217936f4fc6eed5b87d0f8d1a39fe05a05f
vatsalleshwala <vatsalleshwala@gmail.com>
2023-02-16 17:07:45 +0530

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Log.e("TAG","Replace with your own action");
+//                Log.e("TAG","Replace with your own action");
             }
         });
     }
